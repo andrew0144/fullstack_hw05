a6a3205c359dae42fdf92b962380b9017d282c3e
andrew0144 <80224229+andrew0144@users.noreply.github.com>
2022-04-14 18:10:12 -0400

    got some basic work done

--- src/comps/dialog.js
+++ src/comps/dialog.js
@@ -1,47 +1,52 @@
 //imports
-    //react
-        import React from 'react';
-    //materials
-        import Button from '@mui/material/Button';
-        import DialogActions from '@mui/material/DialogActions';
-        import DialogContent from '@mui/material/DialogContent';
-        import DialogTitle from '@mui/material/DialogTitle';
-    //components
-        import DateTime from './dateTime';
+//react
+import React from 'react';
+//materials
+import Button from '@mui/material/Button';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+//components
+import DateTime from './dateTime';
 
 //master export
-    export default function ResponsiveDialog(props) {
+export default function ResponsiveDialog(props) {
     //variables
-        let [deadline] = React.useState(props.dataFromParent.deadline);
+    let [deadline] = React.useState(props.dataFromParent.deadline);
 
     //cancel
-        let cancel = () => {
-            props.parentCallback({
-                action: 'cancel',
-                data: {}
-            });
-        };
+    let cancel = () => {
+        props.parentCallback({
+            action: 'cancel',
+            data: {}
+        });
+    };
 
     //return master object
-        return (
-            <>       
+    return (
+        <>
             {/*title*/}
-                <DialogTitle sx = {{ bgcolor: 'primary.dark', color: 'white'}}>
-                    X
-                </DialogTitle>
+            <DialogTitle sx={{ bgcolor: 'primary.dark', color: 'white' }}>
+                <AddCircleIcon /> Add Task
+            </DialogTitle>
             {/*content*/}
-                <DialogContent>
+            <DialogContent>
                 {/*deadline*/}
-                    <br /><br />
-                    <DateTime dataFromParent = {deadline} dataToParent = {deadline}/>
-                </DialogContent>
+                <br /><br />
+                <DateTime dataFromParent={deadline} dataToParent={deadline} />
+            </DialogContent>
             {/*action buttons*/}
-                <DialogActions sx={{ bgcolor: 'white'}}>
-                    {/*cancel button*/}
-                        <Button onClick = {cancel} variant = "contained" color = 'error' sx = {{bgcolor: '#f44336', width: 100}}>
-                        <i className = "fa fa-fw fa-ban"></i>&nbsp;Cancel
-                    </Button>
-                </DialogActions>
-            </>
-        );
-    }
+            <DialogActions sx={{ bgcolor: 'white' }}>
+                {/*add button*/}
+                <Button onClick={cancel} variant="contained" color='primary' sx={{ width: 100 }}>
+                    <AddCircleIcon />&nbsp;Add
+                </Button>
+                {/*cancel button*/}
+                <Button onClick={cancel} variant="contained" color='error' sx={{ bgcolor: '#f44336', width: 100 }}>
+                    <i className="fa fa-fw fa-ban"></i>&nbsp;Cancel
+                </Button>
+            </DialogActions>
+        </>
+    );
+}
--- src/comps/page.js
+++ src/comps/page.js
@@ -1,92 +1,92 @@
 //imports
-    //react
-        import React, { Component } from "react";
-    //materials
-        import Table from '@mui/material/Table';
-        import TableBody from '@mui/material/TableBody';
-        import TableCell from '@mui/material/TableCell';
-        import TableContainer from '@mui/material/TableContainer';
-        import TableHead from '@mui/material/TableHead';
-        import TableRow from '@mui/material/TableRow';
-        import Button from '@mui/material/Button';
-        import DiaWrap from '@mui/material/Dialog';
-        import Card from '@mui/material/Card';
-        import CardHeader from '@mui/material/CardHeader';
-        import CardContent from '@mui/material/CardContent';
-    //components
-        import Dialog from './dialog';
-    //javascript
-        import moment from 'moment';
-        import toastr from 'toastr';
+//react
+import React, { Component } from "react";
+//materials
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Button from '@mui/material/Button';
+import DiaWrap from '@mui/material/Dialog';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+//components
+import Dialog from './dialog';
+//javascript
+import moment from 'moment';
+import toastr from 'toastr';
 //master export
-    export default class page extends Component {
+export default class page extends Component {
     //constructor
-        constructor(props) {
-            super(props);
-            this.state = {
-                task: {
-                    deadline: moment()
-                },
-                rows: [],
-                open: false
-            };
-        }
+    constructor(props) {
+        super(props);
+        this.state = {
+            task: {
+                deadline: moment()
+            },
+            rows: [],
+            open: false
+        };
+    }
 
     //add task
-        addTask(){
-            this.setState({open: true});
-        };
+    addTask() {
+        this.setState({ open: true });
+    };
 
-   //callback from dialog input
-        dialogCallback = (data) => {//functional syntax intentional
-            if(data.action === `submit`){//submitted
-                toastr.success(`Task added successfully!`, ``, {'closeButton': true, positionClass: 'toast-bottom-right'});
-            }else if(data.action === `cancel`){//cancelled
-                this.setState({open: false});
-            }               
+    //callback from dialog input
+    dialogCallback = (data) => {//functional syntax intentional
+        if (data.action === `submit`) {//submitted
+            toastr.success(`Task added successfully!`, ``, { 'closeButton': true, positionClass: 'toast-bottom-right' });
+        } else if (data.action === `cancel`) {//cancelled
+            this.setState({ open: false });
         }
+    }
 
     //render
-        render() {
-            return (
-                <>
+    render() {
+        return (
+            <>
                 <DiaWrap
                     open={this.state.open}
                     onClose={() => this.dialogCallback()}>
-                    <Dialog 
-                        parentCallback = {this.dialogCallback}
-                        dataFromParent = {this.state.task} >
+                    <Dialog
+                        parentCallback={this.dialogCallback}
+                        dataFromParent={this.state.task} >
                     </Dialog>
                 </DiaWrap>
                 {/*master card*/}
-                    <Card sx = {{ margin: '20px' }}>
+                <Card sx={{ margin: '20px' }}>
                     {/*card header*/}
-                        <CardHeader sx = {{ bgcolor: 'primary.dark', color: 'white'}} 
-                            title = {<><small><i className='fa fa-fw fa-bars'></i>X</small></>}
-                            style = {{textAlign: 'center'}}
-                            action = {
-                                <>
+                    <CardHeader sx={{ bgcolor: 'primary.dark', color: 'white' }}
+                        title={<><small><i className='fa fa-fw fa-bars'></i>FRAMEWORKS</small></>}
+                        style={{ textAlign: 'center' }}
+                        action={
+                            <>
                                 {/*button*/}
-                                    <Button variant = "contained" onClick = {() => this.addTask()} sx = {{width: 100, marginRight: '7px'}}>
-                                        <i className = "fa fa-fw fa-plus-circle"></i>Add
-                                    </Button>
-                                </>
-                            }/>
-                     {/*card content*/}
-                        <CardContent sx = {{ bgcolor: 'white', marginBottom: -1 }}>
-                            <TableContainer>
-                                <Table sx = {{ bgcolor: 'white' }}>
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell align="center" sx = {{ width: 0.1, color: 'gray'}}>X</TableCell>
-                                            <TableCell align="center" sx = {{ width: 0.1, color: 'gray'}}>X</TableCell>
-                                            <TableCell align="center" sx = {{ width: 0.1, color: 'gray'}}>X</TableCell>
-                                            <TableCell align="center" sx = {{ width: 0.1, color: 'gray'}}>X</TableCell>
-                                            <TableCell align="center" sx = {{ width: 0.1, color: 'gray'}}>X</TableCell>
-                                            <TableCell align="center" sx = {{ width: 0.1, color: 'gray'}}>X</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
+                                <Button variant="contained" onClick={() => this.addTask()} sx={{ width: 100, marginRight: '7px' }}>
+                                    <i className="fa fa-fw fa-plus-circle"></i>Add
+                                </Button>
+                            </>
+                        } />
+                    {/*card content*/}
+                    <CardContent sx={{ bgcolor: 'white', marginBottom: -1 }}>
+                        <TableContainer>
+                            <Table sx={{ bgcolor: 'white' }}>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell align="center" sx={{ width: 0.1, color: 'gray' }}>Title</TableCell>
+                                        <TableCell align="center" sx={{ width: 0.1, color: 'gray' }}>Description</TableCell>
+                                        <TableCell align="center" sx={{ width: 0.1, color: 'gray' }}>Deadline</TableCell>
+                                        <TableCell align="center" sx={{ width: 0.1, color: 'gray' }}>Priority</TableCell>
+                                        <TableCell align="center" sx={{ width: 0.1, color: 'gray' }}>Is Complete</TableCell>
+                                        <TableCell align="center" sx={{ width: 0.1, color: 'gray' }}>Action</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
                                     {this.state.rows.map((row, index) => (
                                         <TableRow key={row.title}>
                                             <TableCell align='center'>{row.title}</TableCell>
@@ -97,12 +97,12 @@
                                             <TableCell align="center">X</TableCell>
                                         </TableRow>
                                     ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                        </CardContent>
-                    </Card>
-                </>
-            );
-        }
-    }      
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </CardContent>
+                </Card>
+            </>
+        );
+    }
+}      
